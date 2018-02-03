131c034def320cb776933e35a5db4d514352cfe9
IskraNikolova <air_iskra@yahoo.com>
2018-02-03 09:51:55 +0200

    add new project Simple_Bitcoin_Wallet.sln

---
+++ WalletsTransactionsAndExchanged/Simple_Bitcoin_Wallet/Simple_Bitcoin_Wallet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simple_Bitcoin_Wallet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
